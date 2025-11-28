08816b0e79d46be609d69189a263b3924db71fc6
adisao-hub <aditya40297@gmail.com>
2025-11-29 03:19:02 +0530

    Create D-34 Q-68.c

---
+++ D-34 Q-68.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, pos, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of the array:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter position to delete (1 to %d): ", n);
+    scanf("%d", &pos);
+
+    if(pos < 1 || pos > n) {
+        printf("Invalid position! Deletion not possible.");
+    } else {
+        // Shift elements left to delete the element
+        for(i = pos - 1; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        n--;
+
+        printf("Array after deletion:\n");
+        for(i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
